3ec6df32c24a323da5feea1e001047ab6643d438
Ravikiran Vemula <ravikiran.vemula@gmail.com>
2025-08-14 10:58:24 +0530

    removed executor related code and added constants to store application common variables

--- src/main/java/com/travelapp/availability_service/services/StayAvailabilityService.java
+++ src/main/java/com/travelapp/availability_service/services/StayAvailabilityService.java
@@ -3,11 +3,9 @@ package com.travelapp.availability_service.services;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 import com.travelapp.availability_service.entities.StayRoomMappingDto;
+import com.travelapp.availability_service.util.Constants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
@@ -28,8 +26,6 @@ public class StayAvailabilityService {
     @Autowired
     private StayAvailabilityPersistenceService stayAvailabilityPersistenceService;
 
-    private final ExecutorService executor = Executors.newCachedThreadPool();
-
     public List<StayAvailability> findByKeyStayIdAndKeyDate(@RequestParam String stayId,
                                                             @RequestParam LocalDate startDate, @RequestParam LocalDate endDate) {
         return stayAvailabilityRepository.findByKeyStayIdAndKeyDateBetween(stayId,
@@ -37,7 +33,7 @@ public class StayAvailabilityService {
     }
 
     public void updateBulkAvailability(LocalDate startDate, LocalDate endDate) {
-        String uri = "http://localhost:8090/stay/group-by-city";
+        String uri = Constants.STAY_SERVICE_BASE_URI+"/group-by-city";
         ResponseEntity<Map<String, List<StayRoomMappingDto>>> response =
                 restTemplate.exchange(
                         uri,
@@ -53,17 +49,13 @@ public class StayAvailabilityService {
     }
 
     private void createAvailabilities(LocalDate startDate, LocalDate endDate, Map<String, List<StayRoomMappingDto>> staysByCity) {
-        staysByCity.values().forEach(stays ->
-                CompletableFuture.runAsync(() -> {
+        staysByCity.values().forEach(stays -> {
                     try {
-
                         stayAvailabilityPersistenceService.processCity(startDate, endDate, stays);
-
                     } catch (Exception e) {
                         System.out.println("Error processing city" + e.getMessage());
                     }
-                }, executor)
-
+                }
         );
     }
 }
---
+++ src/main/java/com/travelapp/availability_service/util/Constants.java
@@ -0,0 +1,5 @@
+package com.travelapp.availability_service.util;
+
+public class Constants {
+    public static final String STAY_SERVICE_BASE_URI="http://localhost:8090/stay";
+}
